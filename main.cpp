8388894637f1815dfbfbd249e0db951e446cf42f
farzeen-ali <farzeenaliarif786@gmail.com>
2025-10-08 13:51:09 +0500

    Done By Me

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        int age;
+        float marks;
+
+        void inputData(){
+            cout<<"Enter Your Name: ";
+            getline(cin, name);
+
+            cout<<"Enter your age: ";
+            cin>>age;
+
+            cout<<"Enter your marks: ";
+            cin>>marks;
+        }
+
+        void displayData(){
+            cout<<"Your Name is: "<<name<<endl;
+            cout<<"Your Age is: "<<age<<endl;
+            cout<<"Your Marks is: "<<marks<<endl;
+        }
+    
+    };
+
+int main(){
+    Student s1;
+    s1.inputData();
+    s1.displayData();
+    return 0;
+}
